1a268081aa0ab864df7740263585c3f645c7e9dc
compsci <compsci@compsci>
2017-11-01 10:45:42 -0600

    Work from Nov 1st, 2 methods of arrays

--- src/array_fun/TheMainClass.java
+++ src/array_fun/TheMainClass.java
@@ -3,7 +3,10 @@ package array_fun;
 public class TheMainClass {
 
 	public static void main(String[] args) {
-		
+
+		ArrayFun learnArrays = new ArrayFun();
+		learnArrays.showInstructions();
+		learnArrays.intArrayExample();
 
 	}
 
